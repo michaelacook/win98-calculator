b7cf5cb461e9438d77147e70657db0c8e812b33f
Michael Cook <mcook0775@gmail.com>
2020-05-18 20:00:00 -0400

    Updates

---
+++ precache-manifest.984a6b6faf98df3c195dacdad61c80af.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b128362e8c8ed52111b4241561ec525a",
+    "url": "/win98-calculator/index.html"
+  },
+  {
+    "revision": "23c34e7ee311a65cec41",
+    "url": "/win98-calculator/static/css/2.93dac48e.chunk.css"
+  },
+  {
+    "revision": "c27d749bb4974c81c403",
+    "url": "/win98-calculator/static/css/main.bf7d0126.chunk.css"
+  },
+  {
+    "revision": "23c34e7ee311a65cec41",
+    "url": "/win98-calculator/static/js/2.59f8ab07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/win98-calculator/static/js/2.59f8ab07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27d749bb4974c81c403",
+    "url": "/win98-calculator/static/js/main.88d453dd.chunk.js"
+  },
+  {
+    "revision": "e0e86643dd9b58e5e712",
+    "url": "/win98-calculator/static/js/runtime-main.d67e0f11.js"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "/win98-calculator/static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "7bbbd32e0f9977dd70a4d1287927d5da",
+    "url": "/win98-calculator/static/media/ms_sans_serif.7bbbd32e.woff"
+  },
+  {
+    "revision": "6eb2c63fbead0014ef0c4c537bfe328a",
+    "url": "/win98-calculator/static/media/ms_sans_serif_bold.6eb2c63f.woff"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "/win98-calculator/static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  }
+]);
