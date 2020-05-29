a651faff48a38c1f93aa37a4abc8f30d1467c832
Michael Cook <mcook0775@gmail.com>
2020-05-28 22:19:59 -0400

    Destructure props in MainButtonsGrid

--- src/components/MainButtonsGrid.js
+++ src/components/MainButtonsGrid.js
@@ -7,57 +7,60 @@ import MemoryButton from "./MemoryButton"
 
 class MainButtonsGrid extends Component {
   render() {
+    const {
+      printNumber,
+      division,
+      squareRoot,
+      multiplication,
+      percent,
+      subtraction,
+      reciprocal,
+      addDecimal,
+      addition,
+      compute,
+      addToMemoryStore,
+      clearMemoryStore,
+      addToCurrentMemoryStore,
+      retrieveMemoryStore,
+      toggleNegative,
+    } = this.props
+
     return (
       <div className="main-grid">
         <div className="button-row">
-          <MemoryButton
-            keyFunction={this.props.clearMemoryStore}
-            value={"MC"}
-          />
-          <NumberButton printNumber={this.props.printNumber} value={7} />
-          <NumberButton printNumber={this.props.printNumber} value={8} />
-          <NumberButton printNumber={this.props.printNumber} value={9} />
-          <FunctionButton keyFunction={this.props.division} value={"/"} />
-          <FunctionButton keyFunction={this.props.squareRoot} value={"sqrt"} />
+          <MemoryButton keyFunction={clearMemoryStore} value={"MC"} />
+          <NumberButton printNumber={printNumber} value={7} />
+          <NumberButton printNumber={printNumber} value={8} />
+          <NumberButton printNumber={printNumber} value={9} />
+          <FunctionButton keyFunction={division} value={"/"} />
+          <FunctionButton keyFunction={squareRoot} value={"sqrt"} />
         </div>
 
         <div className="button-row">
-          <MemoryButton
-            keyFunction={this.props.retrieveMemoryStore}
-            value={"MR"}
-          />
-          <NumberButton printNumber={this.props.printNumber} value={4} />
-          <NumberButton printNumber={this.props.printNumber} value={5} />
-          <NumberButton printNumber={this.props.printNumber} value={6} />
-          <FunctionButton keyFunction={this.props.multiplication} value={"*"} />
-          <FunctionButton keyFunction={this.props.percent} value={"%"} />
+          <MemoryButton keyFunction={retrieveMemoryStore} value={"MR"} />
+          <NumberButton printNumber={printNumber} value={4} />
+          <NumberButton printNumber={printNumber} value={5} />
+          <NumberButton printNumber={printNumber} value={6} />
+          <FunctionButton keyFunction={multiplication} value={"*"} />
+          <FunctionButton keyFunction={percent} value={"%"} />
         </div>
 
         <div className="button-row">
-          <MemoryButton
-            keyFunction={this.props.addToMemoryStore}
-            value={"MS"}
-          />
-          <NumberButton printNumber={this.props.printNumber} value={1} />
-          <NumberButton printNumber={this.props.printNumber} value={2} />
-          <NumberButton printNumber={this.props.printNumber} value={3} />
-          <FunctionButton keyFunction={this.props.subtraction} value={"-"} />
-          <FunctionButton keyFunction={this.props.reciprocal} value={"1/x"} />
+          <MemoryButton keyFunction={addToMemoryStore} value={"MS"} />
+          <NumberButton printNumber={printNumber} value={1} />
+          <NumberButton printNumber={printNumber} value={2} />
+          <NumberButton printNumber={printNumber} value={3} />
+          <FunctionButton keyFunction={subtraction} value={"-"} />
+          <FunctionButton keyFunction={reciprocal} value={"1/x"} />
         </div>
 
         <div className="button-row">
-          <MemoryButton
-            keyFunction={this.props.addToCurrentMemoryStore}
-            value={"M+"}
-          />
-          <NumberButton printNumber={this.props.printNumber} value={0} />
-          <FunctionButton
-            keyFunction={this.props.toggleNegative}
-            value={"+/-"}
-          />
-          <FunctionButton keyFunction={this.props.addDecimal} value={"."} />
-          <FunctionButton keyFunction={this.props.addition} value={"+"} />
-          <FunctionButton keyFunction={this.props.compute} value={"="} />
+          <MemoryButton keyFunction={addToCurrentMemoryStore} value={"M+"} />
+          <NumberButton printNumber={printNumber} value={0} />
+          <FunctionButton keyFunction={toggleNegative} value={"+/-"} />
+          <FunctionButton keyFunction={addDecimal} value={"."} />
+          <FunctionButton keyFunction={addition} value={"+"} />
+          <FunctionButton keyFunction={compute} value={"="} />
         </div>
       </div>
     )
